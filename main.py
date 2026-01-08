ab1297d2ab305f6fd4cfbed912801c0ae5dad350
Vedanth-R <vedanth.rao.v@gmail.com>
2026-01-08 13:44:57 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from sentiment import run_sentiment_model
+from Sentiment import run_sentiment_model
 
 app = FastAPI()
 
